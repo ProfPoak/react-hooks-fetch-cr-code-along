5b36a025d5a60557374e02b2085b8a04a32a64ee
Porter <pdhakes25@gmail.com>
2026-02-07 15:00:10 -0700

    render items on page load

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
+
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+    .then(r => {
+      if (r.ok) {
+        return r.json()
+      }
+      else {
+        console.log("fetch request failed")
+      }
+    })
+    .then(items => setItems(items))
+    .catch(error => console.log(error))
+  },[])
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
